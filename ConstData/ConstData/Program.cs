128c40228a0c263bf9b3584f52e43b8236bddc37
Олесуа и Юля <astahova.98@gmail.com>
2018-02-05 10:55:51 +0700

    Константа PI

---
+++ ConstData/ConstData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+ 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** fun with Const *****\n");
+            Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+         
+
+            Console.ReadLine();
+        }
+    }
+}
