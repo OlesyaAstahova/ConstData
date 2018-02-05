672e12550bd1667af7c502be38b02d783050b482
Олесуа и Юля <astahova.98@gmail.com>
2018-02-05 11:27:42 +0700

    Класс MyConst содержаший 2 константы. Одна константа - константное поле, другая - только для чтения

--- ConstData/ConstData/Program.cs
+++ ConstData/ConstData/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConstData
 {
+    class MyConst
+    {
+        public const double E = 2.718;
+        public static readonly double LN10;
+        
+        static MyConst()
+        {
+            LN10 = 2.302;
+        } 
+    }
+
     class MyMathClass
     {
         //public const double PI = 3.14;
@@ -22,8 +33,9 @@ namespace ConstData
         {
             Console.WriteLine("***** fun with Const *****\n");
             Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
-             
-
+            Console.WriteLine("\n***** fun with my Const *****\n");
+            Console.WriteLine("The value of E is: {0}", MyConst.E);
+            Console.WriteLine("\nThe value of LN10 is: {0}", MyConst.LN10);
             Console.ReadLine();
         }
     }
